cf133769712adf2d5aa43921f8a8fc9132c7d3e2
Aaron Ghebretinsae <aaronghebretinsae@gmail.com>
2024-02-07 13:12:01 -0500

    Add Sign up and Login features

---
+++ ecommerce/src/main/java/com/educative/ecommerce/exceptions/AuthenticationFailException.java
@@ -0,0 +1,8 @@
+package com.educative.ecommerce.exceptions;
+
+public class AuthenticationFailException extends IllegalArgumentException {
+    public AuthenticationFailException(String msg){
+        super(msg);
+    }
+
+}
--- ecommerce/src/main/java/com/educative/ecommerce/model/Product.java
+++ ecommerce/src/main/java/com/educative/ecommerce/model/Product.java
@@ -26,7 +26,7 @@ public class Product {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private @NotNull String name;
